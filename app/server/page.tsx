b7133f74a076cc34d9d9eb1eec3502a1fbba6397
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-03-22 18:52:12 +0300

    feat : display a list of all Pokémon with a pagination system for server endpoint

---
+++ app/server/page.tsx
@@ -0,0 +1,10 @@
+import PokemonList from "../../components/PokemonList";
+
+export default function Server() {
+    return (
+        <>
+            <h1>Server Page</h1>
+            <PokemonList />
+        </>
+    )
+}
